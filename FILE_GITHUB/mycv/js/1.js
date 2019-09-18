b3655a7b471ae4b1d1a991a217df236d707fd5d5
vu thanh hieu <vuthanhieu2000@gmail.com>
2019-09-18 23:48:23 +0700

    xong conten3

--- FILE_GITHUB/mycv/js/1.js
+++ FILE_GITHUB/mycv/js/1.js
@@ -1,93 +1,70 @@
 document.addEventListener("DOMContentLoaded",function(){
-   //scroll > 200px ---> disp background : white
-    var menu = document.querySelector('.menu'),
-        text_menu = document.querySelectorAll('ul.navbar-nav li.nav-item a.nav-link'),
-        logo = document.querySelector('.header .navbar a.navbar-brand'),
-        buttom_info = document.querySelector('button.btn.btn-success.show'),
-        content_1 = document.querySelector('.content_1'),
-        content_2 = document.querySelector('.content_2'),
-        profile_menu = document.querySelector('.profile_menu'),
-        local_ct1 = content_1.offsetTop, // vi tris cua content 1
-        local_ct2 = content_2.offsetTop,
-        col_sm_6 = document.querySelectorAll('.content_2 .wrapper .col-lg-6 .col-sm-6'); // vi tris cua content 2
-        
-   window.addEventListener('scroll', function(){
-      if(this.window.pageYOffset>100){
-        menu.classList.add('background');
-        for (let i = 0; i < text_menu.length; i++) {
-            text_menu[i].classList.add('text_green');
-        }
-        logo.classList.add('logo_green');
-      }
-      else{
-        menu.classList.remove('background');
-        for (let i = 0; i < text_menu.length; i++) {
-            text_menu[i].classList.remove('text_green');
-        }
-        logo.classList.remove('logo_green');
-      }
-
-
-      //cuộn chuột phần content_2
-
-      if(this.window.pageYOffset > local_ct2 -500)
-      {
-          col_sm_6[0].classList.add('privous_in');
-          col_sm_6[3].classList.add('next_in');
-      }
-      else{
-        col_sm_6[0].classList.remove('privous_in');
-        col_sm_6[3].classList.remove('next_in');
-      }
-   })
    
- 
-
- var pos = 0,
-     body = document.body, //body cua web
-     html = document.documentElement; // html web
-// funcion về hàm scroll có tham số là vi trí muốn scroll đến
-  function scroll_to_down (location_body_html){
-    var id = setInterval(scroll_content,10);
-
-    function scroll_content(){
-      if(pos >= location_body_html){
-        clearInterval(id);
-      }
-      else{
-        pos = pos + 15;
-        body.scrollTop = pos ; 
-        html.scrollTop = pos ; 
-        
-      }
-    }
-  }
-  function scroll_to_top (location_body_html,to){ 
-    pos = location_body_html;// to : tới đâu
-    var id = setInterval(scroll_content,10);
-
-    function scroll_content(){
-      if(pos <= to){
-        clearInterval(id);
-        pos = location_body_html;
-      }
-      else{
-        pos = pos - 15;
-        body.scrollTop = pos ; 
-        html.scrollTop = pos ; 
-        
-      }
-    }
-  }
-  buttom_info.addEventListener('click',function(){ 
-      scroll_to_top(local_ct1,200);
-  })
-  profile_menu.addEventListener('click',function(){   
-    scroll_to_down(local_ct1);
-    if(pos<=local_ct1){
-      pos = 0;
-    }
-  })
-
 
 },false)
+
+
+$(document).ready(function () {
+
+  //-----phan cuon chuot cho cac page khac nhau cua web----------
+    $('ul li.nav-item:nth-child(1) a.nav-link').click(function (e) { 
+      e.preventDefault();
+      $('body').animate({scrollTop:$('.content_1').offset().top},400);
+      $('html').animate({scrollTop:$('.content_1').offset().top},400); 
+    });
+    $('ul li.nav-item:nth-child(2) a.nav-link').click(function (e) { 
+      e.preventDefault();
+      $('body').animate({scrollTop:$('.content_2').offset().top},400);
+      $('html').animate({scrollTop:$('.content_2').offset().top},400); 
+    });
+    $('ul li.nav-item:nth-child(3) a.nav-link').click(function (e) { 
+      e.preventDefault();
+      $('body').animate({scrollTop:$('.content_3').offset().top},400);
+      $('html').animate({scrollTop:$('.content_3').offset().top},400);     
+    });
+  //------End phan cuon chuot do-------------------------------------
+
+
+  //--------------slide slick cho phan content_3---------------------
+$('.child').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  nextArrow: $('.next'),
+  prevArrow: $('.prevous'),
+  responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+});       
+
+  //--------------end slide slick do----------------------------
+
+
+
+});
